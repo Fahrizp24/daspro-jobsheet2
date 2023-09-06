4981a4937f3ee5f003bc6b340f316d748ceffc23
fahrizp24 <fpradian24@gmail.com>
2023-09-06 11:07:59 +0700

    Simpan kode java ke github

--- praktik 1/Myfirstjavagithub10.java
+++ praktik 1/Myfirstjavagithub10.java
@@ -1,5 +1,5 @@
 public class Myfirstjavagithub10 {
-    public static void public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println(" hello from github");
     }
 }
